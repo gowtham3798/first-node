812bbde83ce3443f2f60ea9ce83c7971fe451620
gowtham <gowtham3798@gmail.com>
2022-01-18 12:00:06 +0530

    pizza

--- index.js
+++ index.js
@@ -5,108 +5,165 @@
 import express from 'express';
 import {MongoClient} from 'mongodb';
 import dotenv from 'dotenv';
-// import { getmoviebyid, deletemovie, addmovie, getAllmovies } from '../helper.js';
-import {moviesRouter} from './routes/movies.js';
+// import {moviesRouter} from './routes/movies.js';
+// import {usersRouter} from './routes/users.js';
+// import bcrypt from 'bcrypt';
 
 dotenv.config();
 console.log(process.env.MONGO_URL)
 
+
+
 const app = express();
-
-const PORT = process.env.PORT;
-
-
-const movies = [
-    {
-    "id": "104",
-    "name": "Interstellar",
-    "poster": "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-    "rating": 8.6,
-    "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
-    "trailer": "https://www.youtube.com/embed/zSWdZVtXT7E",
-    "language": "english"
-    },
-    {
-    "id": "100",
-    "name": "Iron man 2",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-    "rating": 7,
-    "summary": "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-    "trailer": "https://www.youtube.com/embed/wKtcmiifycU",
-    "language": "english"
-    },
-    {
-    "id": "101",
-    "name": "No Country for Old Men",
-    "poster": "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-    "rating": 8.1,
-    "summary": "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-    "trailer": "https://www.youtube.com/embed/38A__WT3-o0",
-    "language": "english"
-    },
-    {
-    "id": "102",
-    "name": "Jai Bhim",
-    "poster": "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-    "summary": "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-    "rating": 8.8,
-    "trailer": "https://www.youtube.com/embed/nnXpbTFrqXA",
-    "language": "tamil"
-    },
-    {
-    "id": "103",
-    "name": "The Avengers",
-    "rating": 8,
-    "summary": "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-    "poster": "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-    "trailer": "https://www.youtube.com/embed/eOrNdBpGMv8",
-    "language": "english"
-    },
-    
-    {
-    "id": "105",
-    "name": "Baahubali",
-    "poster": "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-    "rating": 8,
-    "summary": "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
-    "trailer": "https://www.youtube.com/embed/sOEg_YZQsTI",
-    "language": "telugu"
-    },
-    {
-    "id": "106",
-    "name": "Ratatouille",
-    "poster": "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-    "rating": 8,
-    "summary": "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-    "trailer": "https://www.youtube.com/embed/NgsQ8mVkN8w",
-    "language": "english"
-    }
-    ]
-
-const MONGO_URL = process.env.MONGO_URL;
-
-
-async function createConnection(){
-    const client = new MongoClient(MONGO_URL);
-    await client.connect();
-    console.log("mongo connected12")
-    return client;
-}
-
-export const client = await createConnection();
 
 app.use(express.json())
 
+const PORT = process.env.PORT;
 
-app.get('/' , (req, res) => {
-    res.send('Welcome✅😂')
+app.get("/", (req, res) => {
+    res.send("hello mf")
+})
+const pizza = [{
+    "id": "100",
+    "name": "Veggie Paradise",
+    "image": "https://vegplatter.in/files/public/images/partner/catalog/195/spicy_triple_tango.jpg",
+    "summary": "The awesome foursomee! Golden corn, black olives, capsicum, red paprika",
+    "type":"veg"
+},
+{
+    "id": "101",
+    "name": "Margherita",
+    "image": "https://www.kimscravings.com/wp-content/uploads/2020/01/Margherita-Pizza-5.jpg",
+    "summary": "A classic delight with 100% Real mozzarella cheese",
+    "type":"veg"
+},
+{   
+    "id": "102",
+    "name": "Double Cheese Margherita",
+    "image": "https://buddyspizzaandcafe.com/wp-content/uploads/2021/06/pizza-margherita-recipe.jpg",
+    "summary": "A classic delight loaded with extra 100% real mozzarella cheese",
+    "type":"veg"
+},
+{   
+    "id": "103",
+    "name": "Veg Extravaganza",
+    "image": " https://www.bindowkart.com/product-images/original/55117-veg-extravaganza.jpg",
+    "summary": "Black olives, capsicum, onion, grilled mushroom, corn, tomato, jalapeno & extra cheese",
+    "type":"veg"},
+{   
+       "id": "104",
+        "name": "Cheese n Corn",
+        "image": "https://static.toiimg.com/thumb/59863239.cms?imgsize=284845&width=800&height=800",
+        "summary": "Sweet & Juicy Golden corn and 100% real mozzarella cheese in a delectable combination !",
+        "type":"veg"},
+{   
+            "id": "105",
+            "name": "Chicken Dominator",
+            "image": "https://phyllo.in/images/products/1593851134mhpvnqrxftcodzl7chzx.jpg",
+            "summary": "Loaded with double pepper barbecue chicken, peri-peri chicken, chicken tikka & grilled chicken rashers",
+            "type":"veg"},
+{   
+ "id": "106",
+ "name": "Chicken Golden Delight",
+ "image": "https://www.homemadebakers.in/wp-content/uploads/2020/10/chicken-pizza-300x300.jpg",
+ "summary": "Double pepper barbecue chicken, golden corn and extra cheese, true delight",
+ "type":"nonveg"},
+ {   
+    "id": "107",
+    "name": "Chicken Pepperoni",
+    "image": "https://thumbs.dreamstime.com/z/pizza-chicken-meat-mozzarella-cheese-pepperoni-tomato-olive-salami-pizza-chicken-meat-mozzarella-cheese-pepperoni-tomato-129458486.jpg",
+    "summary": "Double pepper barbecue chicken, golden corn and extra cheese, true delight",
+    "type":"nonveg"},
+    {   
+        "id": "108",
+        "name": "Pepper Barbecue Chicken",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPDQFdVrR8GSElMeHBRm5McfbzTnnYb9HlfA&usqp=CAU",
+        "summary": "Pepper barbecue chicken for that extra zing",
+        "type":"nonveg"},
+        {   
+            "id": "109",
+            "name": "Indi Chicken Tikka",
+            "image": "https://thumbs.dreamstime.com/b/homemade-indian-chicken-tikka-masala-pizza-onions-cilantro-homemade-indian-chicken-tikka-masala-pizza-221590330.jpg",
+            "summary": "The wholesome flavour of tandoori masala with Chicken tikka, onion, red paprika & mint mayo",
+            "type":"nonveg"},
+{   
+    "id": "110",
+    "name": "Bailley One (500ml)",
+    "image": "https://tiimg.tistatic.com/fp/1/004/864/1l-bailley-packaged-drinking-water-184-w300.jpg",
+    "summary": "Packaged Drinking Water",
+    "type":"beverage"},
+    {   
+        "id": "111",
+        "name": "Pepsi (500ml)",
+        "image": "https://static.news.bitcoin.com/wp-content/uploads/2021/12/image-2021-12-09-100057.jpg",
+        "summary": "Sparkling and Refreshing Beverage",
+        "type":"beverage"},
+        {   
+            "id": "112",
+            "name": "Nimbooz ( 350ml )",
+            "image": "https://cdna.artstation.com/p/assets/images/images/021/690/578/large/s-pavithran-nimbooz-poster-design.jpg?1572593581",
+            "summary": "Nimbooz ( 350ml )",
+            "type":"beverage"}]
+        
+            // mongodb+srv://gowtham:<password>@cluster0.um5iv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+            //  const MONGO_URL = "mongodb://localhost"
+
+            const MONGO_URL = process.env.MONGO_URL;
+
+            async function createConnection(){
+                const client = new MongoClient(MONGO_URL)
+                await client.connect()
+                return client;
+            }
+            
+   const client = await createConnection()
+
+app.get("/pizzas",async (req, res) => {
+    const {type,name} = req.query;
+   console.log(req.query ,type );
+   
+//    db.pizza.find({})
+
+   const result =await client.db("firstmongo")
+   .collection("pizza")
+   .find(req.query)
+   .toArray()
+   res.send(result)
+
 })
 
-app.use("/movies",moviesRouter)
 
-      
+   app.get("/pizzas/:id",async(req, res) => {
+       const{id} = req.params;
+       console.log(id);
+       const result =await client.db("firstmongo")
+       .collection("pizza")
+       .findOne({id:id})
+       res.send(result)
 
-app.listen(PORT, () => console.log('server start',PORT));
+       result ? response.send(result) : response.status(404).send({message:"no items found"})
+   })
 
+   app.delete("/pizzas/:id",async(req, res) => {
+    const{id} = req.params;
+    console.log(id);
+    const result =await client.db("firstmongo")
+    .collection("pizza")
+    .findOne({id:id})
+    res.send(result)
 
+    result ? res.send(result) : res.send({message:"no items found"})
+})
+
+app.post("/pizzas" , async (req, res) => {
+    const newPizza = req.body;
+        console.log(newPizza)
+        // const movie = movies.find((mvs) => mvs.id === req.params.id)
+        const movie = await client.db("firstmongo")
+        .collection("pizza")
+        .insertMany(newPizza);
+        res.send(movie) 
+    })
+
+            app.listen(PORT,() => console.log("server started",PORT));
 
